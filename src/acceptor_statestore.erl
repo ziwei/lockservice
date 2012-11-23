5531757244d3accd9bc0b52686c12094ecdce4fa
Ziwei <ziweichen88@gmail.com>
2012-11-24 00:27:46 +0100

    simpacceptor

---
+++ src/acceptor_statestore.erl
@@ -0,0 +1,30 @@
+-module(acceptor_statestore).
+
+-export([
+         init/0,
+         find/1,
+         replace/1,
+         add/1,
+        ]).
+
+-include_lib("stdlib/include/ms_transform.hrl").
+-include_lib("acceptor_state.hrl").
+
+-define (TABLE, acceptor_statestore).
+
+init() -> %TODO: make this private; it is public for delete in test cleanup 
+    ets:new(?TABLE, [set, named_table, public]). 
+
+find(ElectionId) ->
+    case ets:lookup(?TABLE, ElectionId) of
+        [] ->
+            false;
+        [{_ElectionId, _Election}=Found] ->
+            Found
+    end.
+
+replace(NewElection) ->
+    add(NewElection).
+
+add(Election) ->
+    ets:insert(?TABLE, {Election#election.id, Election}).
--- src/simple_acceptor.erl
+++ src/simple_acceptor.erl
@@ -10,19 +10,33 @@
 %% --------------------------------------------------------------------
 %% Include files
 %% --------------------------------------------------------------------
-
+-include_lib("acceptor_state.hrl").
 %% --------------------------------------------------------------------
 %% External exports
 -export([]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, 
+        terminate/2, code_change/3]).
 
 -record(state, {}).
 
 %% ====================================================================
 %% External functions
 %% ====================================================================
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:cast(?MODULE, stop);
+stop(Name) ->
+    gen_server:cast(Name, stop).
+
+prepare(Acceptor, {Election,Round}) ->
+  gen_server:call(Acceptor, {prepare, {Election, Round}}).
+
+accept(Acceptor, {Election,Round}, Value) ->
+  gen_server:call(Acceptor, {accept, {Election, Round}, Value}).
 
 
 %% ====================================================================
@@ -38,41 +52,85 @@
 %%          {stop, Reason}
 %% --------------------------------------------------------------------
 init([]) ->
-    {ok, #state{}}.
+	acceptor_statestore:init(),
+    StartState = #state{},
+    {ok, StartState}.
 
-%% --------------------------------------------------------------------
-%% Function: handle_call/3
-%% Description: Handling call messages
-%% Returns: {reply, Reply, State}          |
-%%          {reply, Reply, State, Timeout} |
-%%          {noreply, State}               |
-%%          {noreply, State, Timeout}      |
-%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
-handle_call(Request, From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+handle_call({prepare, {ElectionId, Round}}, _From, State) ->
+    handle_prepare({ElectionId, Round}, State);
+handle_call({accept, {ElectionId, Round}, Value}, _From, State) ->
+    handle_accept({ElectionId, Round}, Value, State).
 
-%% --------------------------------------------------------------------
-%% Function: handle_cast/2
-%% Description: Handling cast messages
-%% Returns: {noreply, State}          |
-%%          {noreply, State, Timeout} |
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
-handle_cast(Msg, State) ->
-    {noreply, State}.
+handle_cast(stop, State) -> {stop, normal, State}.
 
-%% --------------------------------------------------------------------
-%% Function: handle_info/2
-%% Description: Handling all non call/cast messages
-%% Returns: {noreply, State}          |
-%%          {noreply, State, Timeout} |
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
 handle_info(Info, State) ->
     {noreply, State}.
+%%%===================================================================
+%%% Prepare requests
+%%%=================================================================== 
+handle_prepare({ElectionId, Round}, State) ->
+    case acceptor_statestore:find(ElectionId) of
+        {ElectionId, FoundElection} ->
+            handle_prepare_for_existing_election(ElectionId, Round, FoundElection, State);
+        false ->
+            create_new_election_from_prepare_request(ElectionId, Round, State)
+    end.    
+
+handle_prepare_for_existing_election(_ElectionId, Round, FoundElection, State) ->
+    HighestPromise = max(Round, FoundElection#election.promised),
+    NewElection = FoundElection#election{promised = HighestPromise},
+    update_election(NewElection),
+    Reply = {promised, HighestPromise, NewElection#election.accepted},
+    %persister:remember_promise(ElectionId, NewElection#election.promised),
+    {reply, Reply, State}.
+
+create_new_election_from_prepare_request(ElectionId, Round, State) ->
+    NewElection = #election{id = ElectionId, promised = Round},
+    add_new_election(NewElection),
+    %persister:remember_promise(ElectionId, Round),
+    {reply, {promised, Round, NewElection#election.accepted}, State}.
+
+%%%===================================================================
+%%% Accept requests
+%%%=================================================================== 
+handle_accept({ElectionId, Round}, Value, State) ->
+    {Reply, NextState} = 
+        case acceptor_statestore:find(ElectionId) of
+            {ElectionId, _ElectionRecord}=Election ->
+                handle_accept_for_election(Round, Value, Election, State);
+            false ->
+                create_new_election_from_accept_request(ElectionId, Round, Value, State)
+        end,
+    {reply, Reply, NextState}.
+
+handle_accept_for_election(Round, Value, {ElectionId, Election}, State) 
+  when Round >= Election#election.promised ->
+    NewElection = Election#election{id = ElectionId, 
+                                    promised = Round, 
+                                    accepted = {Round, Value}},
+    update_election(NewElection),
+    %persister:remember_vote(ElectionId, Round, Value),
+    {{accepted, Round, Value}, State};
+handle_accept_for_election(Round, _Value, _Election, State) ->
+    {{reject, Round}, State}.
+
+create_new_election_from_accept_request(ElectionId, Round, Value, State) ->
+    NewElection = #election{id = ElectionId, 
+                            promised = Round,
+                            accepted = {Round, Value}},
+    add_new_election(NewElection),
+    %persister:remember_vote(ElectionId, Round, Value),
+    {{accepted, Round, Value}, State}.
+
+
+%%%===================================================================
+%%% Internal functions
+%%%=================================================================== 
+add_new_election(NewElection) ->
+    acceptor_statestore:add(NewElection).
+
+update_election(NewElection) ->
+    acceptor_statestore:replace(NewElection).
 
 %% --------------------------------------------------------------------
 %% Function: terminate/2
